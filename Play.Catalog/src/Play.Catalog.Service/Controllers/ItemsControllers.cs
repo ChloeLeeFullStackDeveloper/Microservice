23da29edcb348408e9119faaae9bc0553f7a8328
ChloeLeeFullStackDeveloper <chloe.lee0436@gmail.com>
2025-02-08 22:40:07 -0700

    handling invalid inputs

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsControllers.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsControllers.cs
@@ -22,8 +22,13 @@ namespace Play.Catalog.Service.Controllers
     }
 
     [HttpGet("{id}")]
-    public ItemDto GetById(Guid id) {
+    public ActionResult<ItemDto> GetById(Guid id) {
       var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+      if (item == null) 
+      {
+        return NotFound();
+      }
       return item;
     }
 
@@ -41,6 +46,10 @@ namespace Play.Catalog.Service.Controllers
     {
       var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
 
+      if (existingItem == null) {
+        return NotFound();
+      }
+
       var updatedItem = existingItem with {
         Name = updateItemDto.Name,
         Description = updateItemDto.Description,
@@ -57,6 +66,11 @@ namespace Play.Catalog.Service.Controllers
     public IActionResult Delete(Guid id)
     {
       var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+      if (index < 0) {
+        return NotFound();
+      }
+      
       items.RemoveAt(index);
 
       return NoContent();
